5ac95c1381a6320a01747f764d6c40f4c869fb86
Alexandr Laketych <alaketych@ukr.net>
2018-06-13 23:04:47 +0300

    without animation

---
+++ SwitchButton/Switcher.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwitchButton
+{
+    public partial class Switcher : Window
+    {
+        public Switcher()
+        {
+            InitializeComponent();
+        }
+
+        bool activated = false;
+        Point point;
+
+        private void CircleKnob_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            activated = true;
+            point = Mouse.GetPosition(CircleKnob);
+            Mouse.Capture(CircleKnob);
+        }
+
+        private void CircleKnob_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (activated)
+            {
+                double movement = Canvas.GetLeft(CircleKnob) + Mouse.GetPosition(CircleKnob).X - point.X;
+                Canvas.SetLeft(CircleKnob, movement);
+            }
+        }
+
+        private void CircleKnob_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            activated = false;
+            Mouse.Capture(null);
+        }
+    }
+}
